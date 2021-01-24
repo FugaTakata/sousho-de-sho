34f287359e9ac77fad2253dbdeeb6d59b054888e
FugaTakata <maple1213goo@gmail.com>
2021-01-24 15:40:47 +0900

    use ts

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,9 +1,13 @@
 import Layout from "../../components/Layout";
+import Link from "next/link";
 
 export default function Create() {
   return (
     <Layout>
       <div>create page</div>
+      <Link href="/">
+        <a>to home</a>
+      </Link>
     </Layout>
   );
 }
