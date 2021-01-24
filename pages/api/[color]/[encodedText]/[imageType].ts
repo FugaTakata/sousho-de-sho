94071119d611bbff42d0ad568d7cd25642d3c650
FugaTakata <maple1213goo@gmail.com>
2021-01-24 22:20:16 +0900

    optimize drawer

--- pages/api/[color]/[encodedText]/[imageType].ts
+++ pages/api/[color]/[encodedText]/[imageType].ts
@@ -2,6 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import * as path from "path";
 import { createCanvas, registerFont, loadImage } from "canvas";
 
+type DrawArea = {
+  width: number;
+  height: number;
+};
+
 const colors: Array<string> = [
   "black",
   "blue",
@@ -21,8 +26,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const width = sizes[req.query.imageType as string]?.width | 600;
   const height = sizes[req.query.imageType as string]?.height | 315;
 
-  const encodedText = req.query.encodedText as string;
+  const drawArea: DrawArea = {
+    width: (width / 5) * 4,
+    height: (height / 5) * 4,
+  };
+
+  const text = req.query.encodedText as string;
   let color = req.query.color as string;
+
+  const lines = text.split("\n");
+  let maxLength = 0;
+  lines.forEach((line) => {
+    if (maxLength < line.length) {
+      maxLength = line.length;
+    }
+  });
+
+  const space = {
+    width: drawArea.width / maxLength,
+    height: drawArea.height / lines.length,
+  };
+  const charSize = Math.min(space.width, space.height);
 
   if (!colors.includes(color)) {
     const i = Math.floor(Math.random() * colors.length);
@@ -41,15 +65,41 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   );
   context.drawImage(backgroundImage, 0, 0, width, height);
 
-  context.font = "40px gatasosyo";
+  context.font = `${charSize}px gatasosyo`;
   context.fillStyle = "#424242";
-  context.textAlign = "start";
+  context.textAlign = "center";
   context.textBaseline = "middle";
-  context.fillText(
-    `${decodeURIComponent(encodedText)}`,
-    width / 20,
-    height / 20
-  );
+  // const lines = createTextLines(context, text);
+  // lines.forEach((line, index) => {
+  //   const y = 157 + (40 * (lines.length - 1)) / 2;
+  //   context.fillText(line, 300, y);
+  // });
+  lines.forEach((line, lineIndex) => {
+    const charArray: string[] = line.split("");
+    charArray.forEach((char, charIndex) => {
+      if (space.height < space.width) {
+        context.fillText(
+          char,
+          width / 10 +
+            (space.width / 2) * (maxLength - line.length + 1) +
+            space.width * charIndex,
+          height / 10 + charSize / 2 + charSize * lineIndex
+        );
+      } else {
+        context.fillText(
+          char,
+          width / 10 + charSize / 2 + charSize * charIndex,
+          height / 10 + space.height / 2 + space.height * lineIndex
+        );
+      }
+    });
+    // context.fillText(
+    //   line,
+    //   width / 2,
+    //   height / 10 + charSize / 2 + charSize * index
+    // );
+  });
+  // context.fillText(text, width / 20, height / 20);
 
   const buffer = canvas.toBuffer();
 
