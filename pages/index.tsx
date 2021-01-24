8ba4c33a23fa9639d1278e26bdcfa395193424a3
FugaTakata <maple1213goo@gmail.com>
2021-01-24 15:41:44 +0900

    use Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <main>
         <h1>Home</h1>
         <Link href="/create">
           <a>to create</a>
         </Link>
       </main>
-    </div>
+    </Layout>
   );
 }
