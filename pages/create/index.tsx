090503637e792d675a7b9828c6fe3981a43b095e
FugaTakata <maple1213goo@gmail.com>
2021-01-24 17:10:30 +0900

    feature add textarea

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,13 +1,46 @@
 import Layout from "../../components/Layout";
-import Link from "next/link";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 
 export default function Create() {
+  const router = useRouter();
+  const [text, setText] = useState("");
+  const [color, setColor] = useState("pink");
+
+  function onFormSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const encodedText = encodeURIComponent(text);
+
+    router.push(`/preview/${color}/${encodedText}`);
+  }
+
   return (
     <Layout>
-      <div>create page</div>
-      <Link href="/">
-        <a>to home</a>
-      </Link>
+      <div className="text-center">
+        <h2>草書の画像を作成する</h2>
+        <p className="m-5">入力した言葉を草書にしましょう！</p>
+        <form onSubmit={onFormSubmit}>
+          <h3>言葉の入力</h3>
+          <textarea
+            className="form-control"
+            placeholder={`やってみせ、言って聞かせて、
+させてみせ、褒めてやらねば人は動かじ`}
+            rows={6}
+            required
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div className="m-3">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={text.length === 0}
+            >
+              草書にする
+            </button>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 }
