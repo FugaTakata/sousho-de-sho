f9ab103d6b962dd3eeb19abc776b3d81a39ed04d
FugaTakata <maple1213goo@gmail.com>
2021-01-24 14:19:10 +0900

    fix useRouter returns undefined

--- pages/preview/[color]/[encodedText]/index.tsx
+++ pages/preview/[color]/[encodedText]/index.tsx
@@ -1,18 +1,26 @@
+import { route } from "next/dist/next-server/server/router";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Layout from "../../../../components/Layout";
 
 const colors = ["black", "blue", "green", "indigo", "orange", "pink", "red"];
 
 export default function Preview() {
   const router = useRouter();
-  let color = router.query.color as string;
-  const encodedText = router.query.encodedText as string;
+  const [color, setColor] = useState<string>("");
+  const [encodedText, setEncodedText] = useState<string>("");
 
-  if (!colors.includes(color)) {
-    const i = Math.floor(Math.random() * colors.length);
-    color = colors[i];
-  }
+  useEffect(() => {
+    // idがqueryで利用可能になったら処理される
+    if (router.asPath !== router.route) {
+      setEncodedText(router.query.encodedText as string);
+      if (!colors.includes(router.query.encodedText as string)) {
+        const i = Math.floor(Math.random() * colors.length);
+        setColor(colors[i]);
+      }
+    }
+  }, [router]);
 
   const ogpImageUrl = `${process.env.NEXT_PUBLIC_WEB_URL}/api/${color}/${encodedText}/ogp`;
 
